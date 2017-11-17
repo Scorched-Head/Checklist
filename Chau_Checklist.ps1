b8e0951f51282983f69df6de363e35b6746906af
Scorch <adriangmagno@gmail.com>
2017-11-17 15:17:51 -0300

    Actualización de Script acorde a la planilla Checklist_V2

--- Chau_Checklist.ps1
+++ Chau_Checklist.ps1
@@ -1,6 +1,6 @@
 ﻿$server = Read-Host -Prompt "Nombre del server"
 $xl=New-Object -ComObject "Excel.Application"
-$wb=$xl.Workbooks.Open($env:USERPROFILE+"\Desktop\Checklist.xlsx")
+$wb=$xl.Workbooks.Open($env:USERPROFILE+"\Desktop\CheckLists_V2.xlsx")
 $ws=$wb.ActiveSheet
 $xl.Visible=$True
 $cells=$ws.Cells
@@ -42,37 +42,56 @@ Select @{n='Server';e={$_.SystemName}},
  } 
             
 
+#DATOS DEL SERVER
+			
 $cells.item(5,3)=($server)
 $cells.item(7,3)=(get-Date)
 $cells.item(4,3)=(GWMI Win32_OperatingSystem -ComputerName $server | % {$_.Caption})
-$cells.item(11,6)=(GWMI –Class Win32_Processor -Computername $server | measure -Property NumberOfLogicalProcessors -sum).sum,' CPUs' -join "`r"
-$cells.item(12,6)=(Get-WmiObject -Class win32_operatingsystem -Computername $server | % {[math]::round($_.TotalVisibleMemorySize /1MB)}),' GB' -join "`r"
-$cells.item(13,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DeviceID = 'C:'" | % {$_.DeviceID})
-$cells.item(14,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DriveType=3" | Where {$_.DeviceID -eq 'E:'} | % {$_.DeviceID})
-$cells.item(15,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DriveType=3" | Where {$_.DeviceID -eq 'F:'} | % {$_.DeviceID})
-$cells.item(16,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DeviceID = 'D:'" | % {$_.DeviceID})
 
+#PARAMETROS
 
-#$cells.item(17,6)=(Get-InfoNIC | % {$_.IP})
+$cells.item(13,6)=(GWMI –Class Win32_Processor -Computername $server | measure -Property NumberOfLogicalProcessors -sum).sum,' CPUs' -join "`r"
+$cells.item(14,6)=(Get-WmiObject -Class win32_operatingsystem -Computername $server | % {[math]::round($_.TotalVisibleMemorySize /1MB)}),' GB' -join "`r"
+$cells.item(15,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DeviceID = 'C:'" | % {$_.DeviceID})
+$cells.item(16,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DriveType=3" | Where {$_.DeviceID -eq 'E:'} | % {$_.DeviceID})
+$cells.item(17,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DriveType=3" | Where {$_.DeviceID -eq 'F:'} | % {$_.DeviceID})
+$cells.item(18,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DeviceID = 'D:'" | % {$_.DeviceID})
+#$cells.item(19,6)=(Get-InfoNIC | % {$_.IP})
+$cells.item(24,6)=(GWMI -Class CIM_DataFile -ComputerName $server -Filter "Name='C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe'" | % {$_.Version})
+$cells.item(25,6)=(GWMI -Class CIM_DataFile -ComputerName $server -Filter "Name='C:\\Program Files (x86)\\Symantec\\Symantec Endpoint Protection\\Smc.exe'" | % {$_.Version})
+$cells.item(26,6)=(Invoke-Command -ComputerName $server -ScriptBlock {Get-ItemProperty -Path "HKLM:\SOFTWARE\Symantec\Symantec Endpoint Protection\CurrentVersion\public-opstate"} | % {$_.LatestVirusDefsDate})
+$cells.item(27,6)=(Get-Service -ComputerName $server -Name SepMasterService | % {$_.Status}), '' -join "`r"
+$cells.item(28,6)=(GWMI -Class CIM_DataFile -ComputerName $server -Filter "Name='C:\\Program Files\\Altiris\\Altiris Agent\\AeXNSAgent.exe'" | % {$_.Version})
+$cells.item(29,6)=(Get-Service -ComputerName $server -Name AeXNSClient | % {$_.Status}), '' -join "`r"
 
-$cells.item(22,6)=(GWMI -Class CIM_DataFile -ComputerName $server -Filter "Name='C:\\Program Files\\VMware\\VMware Tools\\vmtoolsd.exe'" | % {$_.Version})
-$cells.item(23,6)=(GWMI -Class CIM_DataFile -ComputerName $server -Filter "Name='C:\\Program Files (x86)\\Symantec\\Symantec Endpoint Protection\\Smc.exe'" | % {$_.Version})
-$cells.item(24,6)=(Invoke-Command -ComputerName $server -ScriptBlock {Get-ItemProperty -Path "HKLM:\SOFTWARE\Symantec\Symantec Endpoint Protection\CurrentVersion\public-opstate"} | % {$_.LatestVirusDefsDate})
-$cells.item(25,6)=(Get-Service -ComputerName $server -Name SepMasterService | % {$_.Status}), '' -join "`r"
-$cells.item(26,6)=(GWMI -Class CIM_DataFile -ComputerName $server -Filter "Name='C:\\Program Files\\Altiris\\Altiris Agent\\AeXNSAgent.exe'" | % {$_.Version})
-$cells.item(27,6)=(Get-Service -ComputerName $server -Name AeXNSClient | % {$_.Status}), '' -join "`r"
-$cells.item(31,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DeviceID = 'C:'" | % {$_.BlockSize=(($_.FreeSpace)/($_.Size))*100;
+#ALMACENAMIENTO
+
+$cells.item(33,5)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DeviceID = 'C:'" | % {$_.BlockSize=(($_.FreeSpace)/($_.Size))*100;
    $_.FreeSpace=($_.FreeSpace/1GB);$_} | % {$_.BlockSize}),' % Libre' -join "`r"
-$cells.item(32,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DeviceID = 'D:'" | % {$_.BlockSize=(($_.FreeSpace)/($_.Size))*100;
+$cells.item(34,5)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DeviceID = 'D:'" | % {$_.BlockSize=(($_.FreeSpace)/($_.Size))*100;
    $_.FreeSpace=($_.FreeSpace/1GB);$_} | % {$_.BlockSize}),' % Libre' -join "`r"
-$cells.item(33,6)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DriveType=3" | Where {$_.DeviceID -eq 'E:'} | % {$_.BlockSize=(($_.FreeSpace)/($_.Size))*100;
+$cells.item(35,5)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DriveType=3" | Where {$_.DeviceID -eq 'E:'} | % {$_.BlockSize=(($_.FreeSpace)/($_.Size))*100;
    $_.FreeSpace=($_.FreeSpace/1GB);$_} | % {$_.BlockSize}),' % Libre' -join "`r"
-$cells.item(37,6)=(GWMI –class Win32_processor -Computername $server | measure -Property LoadPercentage -Average).Average,' % Utilizado' -join "`r"
-$cells.item(38,6)=(Get-WmiObject -Class win32_operatingsystem -Computername $server| 
+$cells.item(36,5)=(GWMI Win32_LogicalDisk -Computername $server -Filter "DriveType=3" | Where {$_.DeviceID -eq 'F:'} | % {$_.BlockSize=(($_.FreeSpace)/($_.Size))*100;
+   $_.FreeSpace=($_.FreeSpace/1GB);$_} | % {$_.BlockSize}),' % Libre' -join "`r"
+   
+#DESEMPEÑO
+ 
+$cells.item(39,6)=(GWMI –class Win32_processor -Computername $server | measure -Property LoadPercentage -Average).Average,' % Utilizado' -join "`r"
+$cells.item(40,6)=(Get-WmiObject -Class win32_operatingsystem -Computername $server| 
 % {[math]::round(($_.FreePhysicalMemory)/($_.TotalVisibleMemorySize)*100,2)}),' % Libre' -join "`r"
-$cells.item(41,6)=(Get-Ciminstance -Class "Win32_UserAccount" -ComputerName $server | Where-Object {$_.Name -like '*localadmin*'} | % {$_.Name})
-$cells.item(46,6)=(GWMI -Class CIM_DataFile -ComputerName $server -Filter "Name='C:\\Program Files\\Veritas\\NetBackup\\bin\\NbWin.exe'" | % {$_.Version})
-$cells.item(50,6)=(Invoke-Command -ComputerName $server -ScriptBlock {Get-ScheduledTask -TaskName Backup*}|% {$_.TaskName})
+
+#SEGURIDAD
+
+$cells.item(43,6)=(Get-Ciminstance -Class "Win32_UserAccount" -ComputerName $server | Where-Object {$_.Name -like '*localadmin*'} | % {$_.Name})
+
+#BACKUP/RESTORE
+
+$cells.item(48,6)=(GWMI -Class CIM_DataFile -ComputerName $server -Filter "Name='C:\\Program Files\\Veritas\\NetBackup\\bin\\NbWin.exe'" | % {$_.Version})
+
+#OTROS
+
+$cells.item(52,6)=(Invoke-Command -ComputerName $server -ScriptBlock {Get-ScheduledTask -TaskName Backup*}|% {$_.TaskName})
 
 
 $wb.SaveAs($env:USERPROFILE+"\Desktop\$server.xlsx")
